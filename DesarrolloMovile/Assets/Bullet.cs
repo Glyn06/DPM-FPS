0a36bf0be3a1b2cb2c2df2819b91b3924fa88193
Unknown <jquintanamartin@hotmail.com>
2018-08-28 10:46:37 -0300

    Bullet + Weapon rotation

---
+++ DesarrolloMovile/Assets/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += dir;
+	}
+
+    public void Fire(Vector3 _dir) {
+        dir = _dir;
+    }
+}
--- DesarrolloMovile/Assets/Scripts/Weapon.cs
+++ DesarrolloMovile/Assets/Scripts/Weapon.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour {
 
+    public Bullet bullet;
+    public Camera cam;
+
+    private void Update()
+    {
+        gameObject.transform.rotation = cam.transform.rotation;
+    }
+
     public void Fire() {
-        Debug.Log("Fire from: " + gameObject.name);
+        Bullet instance;
+        instance = Instantiate(bullet, transform.position, Quaternion.identity, null);
+        instance.Fire(transform.forward);
     }
 }
