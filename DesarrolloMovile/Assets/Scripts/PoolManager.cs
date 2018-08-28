a21d857ebb00d08f5b1e74492b3b40672ad30713
Unknown <jquintanamartin@hotmail.com>
2018-08-28 11:02:20 -0300

    Pool Manager

---
+++ DesarrolloMovile/Assets/Scripts/PoolManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour {
+
+    public Bullet bulletPrefab;
+    public int cantBullets = 10;
+
+    List<Bullet> bullets = new List<Bullet>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < cantBullets; i++)
+        {
+            bullets.Add(bulletPrefab);
+        }
+        for (int i = 0; i < cantBullets; i++)
+        {
+            GameObject bullet;
+            bullet = Instantiate(bullets[i].gameObject);
+            bullet.SetActive(false);
+        }
+    }
+
+
+}
