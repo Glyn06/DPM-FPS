217d7537d17e6c52f77557664a410ef0e69db31d
Unknown <lean1998@hotmail.com>
2018-09-09 01:36:27 -0300

    EnemyPathfindingSystem

--- DesarrolloMovile/Assets/Scripts/Damageable/Enemy.cs
+++ DesarrolloMovile/Assets/Scripts/Damageable/Enemy.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
     public int life;
+    public NavMeshAgent enemyAgent;
+    private Player playerInstance;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        playerInstance = Player.instance;
+        enemyAgent = GetComponent<NavMeshAgent>();
+        enemyAgent.SetDestination(playerInstance.transform.position);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
