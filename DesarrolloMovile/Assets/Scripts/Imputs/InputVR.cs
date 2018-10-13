865a935ca4cfafb86205bb98f1832098ecc8afae
Unknown <jquintanamartin@hotmail.com>
2018-10-13 10:57:00 -0300

    Updated to Unity 2018

--- DesarrolloMovile/Assets/Scripts/Imputs/InputVR.cs
+++ DesarrolloMovile/Assets/Scripts/Imputs/InputVR.cs
@@ -4,63 +4,25 @@ using UnityEngine;
 
 public class InputVR : MonoBehaviour, InputInterface
 {
-    public VRTK.VRTK_ControllerEvents VRRightController;
-    public VRTK.VRTK_ControllerEvents VRLeftController;
 
     private bool onRightTriggerDown = false;
     private bool onLeftTriggerDown = false;
 
     public bool LeftFireButton()
     {
-
-        if (VRRightController.triggerPressed && !onRightTriggerDown)
-        {
-            onRightTriggerDown = true;
-            return true;
-        }
-        
-
-        if(!VRRightController.triggerPressed && onRightTriggerDown)
-        {
-            onRightTriggerDown = false;
-            return false;
-        }
-
         return false;
     }
 
     public bool RightFireButton()
     {
-
-        if (VRLeftController.triggerPressed && !onLeftTriggerDown)
-        {
-            onLeftTriggerDown = true;
-            return true;
-        }
-
-
-        if (!VRLeftController.triggerPressed && onLeftTriggerDown)
-        {
-            onLeftTriggerDown = false;
-            return false;
-        }
-
         return false;
     }
 
     public bool LeftReloadButton() {
-        if (VRLeftController.buttonOnePressed)
-        {
-            return true;
-        }
         return false;
     }
 
     public bool RightReloadButton() {
-        if (VRRightController.buttonOnePressed)
-        {
-            return true;
-        }
         return false;
     }
 }
