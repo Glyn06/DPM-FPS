f2678d4b35c7219ed19bcc1b79caa9c02a9d1bc5
Unknown <jquintanamartin@hotmail.com>
2018-11-06 09:59:06 -0300

    Maximun enemies perwave

--- DesarrolloMovile/Assets/Scripts/WaveManger.cs
+++ DesarrolloMovile/Assets/Scripts/WaveManger.cs
@@ -6,13 +6,23 @@ public class WaveManger : MonoBehaviour {
 
     public List<Spawner> spawners;
     public float spawningTime;
+    public int enemiesOnFirstWave = 4;
 
     private float timer;
+    private float wave = 1;
+    private int enemiesCount;
+    private int enemiesPerWave;
+
+    private void Start()
+    {
+        enemiesPerWave = enemiesOnFirstWave;
+    }
+
 
     private void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= spawningTime)
+        if (timer >= spawningTime && enemiesCount < enemiesPerWave)
         {
             SpawnAt(Random.Range(0,spawners.Count));
             timer = 0;
@@ -21,5 +31,6 @@ public class WaveManger : MonoBehaviour {
 
     void SpawnAt(int spawnerIndex) {
         spawners[spawnerIndex].Spawn();
+        enemiesCount++;
     }
 }
