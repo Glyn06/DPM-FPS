40c2a0e061250e5ea3abe56a9d06154f8470fd26
Unknown <jquintanamartin@hotmail.com>
2018-11-13 09:53:57 -0300

    WaveManagerPrefab

--- DesarrolloMovile/Assets/Scripts/WaveManager.cs
+++ DesarrolloMovile/Assets/Scripts/WaveManager.cs
@@ -18,11 +18,13 @@ public class WaveManager : MonoBehaviour {
     #endregion
 
     public List<Spawner> spawners;
-    public float spawningTime;
+    public float minSpawningTime;
+    public float maxSpawningTime;
     public int enemiesOnFirstWave = 4;
     public int enemiesAdded = 2;
 
     private float timer;
+    private float spawningTime;
     private int wave = 1;
     private int enemiesCount;
     private int spawnedEnemiesCount;
@@ -32,6 +34,7 @@ public class WaveManager : MonoBehaviour {
     {
         enemiesPerWave = enemiesOnFirstWave;
         enemiesCount = enemiesOnFirstWave;
+        spawningTime = Random.Range(minSpawningTime, maxSpawningTime);
     }
 
 
@@ -41,6 +44,7 @@ public class WaveManager : MonoBehaviour {
         if (timer >= spawningTime && spawnedEnemiesCount < enemiesPerWave)
         {
             SpawnAt(Random.Range(0,spawners.Count));
+            spawningTime = Random.Range(minSpawningTime, maxSpawningTime);
             timer = 0;
         }
 
@@ -60,8 +64,6 @@ public class WaveManager : MonoBehaviour {
         enemiesPerWave += enemiesAdded;
         enemiesCount = enemiesPerWave;
         spawnedEnemiesCount = 0;
-
-        Debug.Log("Next Wave");
     }
 
     public void OnEnemyDown() {
